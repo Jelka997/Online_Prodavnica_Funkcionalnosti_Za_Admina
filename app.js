9f6df562f921575e0b4f8b29b897bc4be3c52b6d
Jelena Stanojoski <stanojoskijelena997@gmail.com>
2025-09-16 18:00:48 +0200

    Dodat sadrzaj da se na klik artikla u okviru tabele renderuju detalji o artiklu

--- app.js
+++ app.js
@@ -16,11 +16,16 @@ function kreirajRed() {
 
     for (let i = 0; i < artikli.length; i++) {
         let tr = document.createElement("tr")
-        
+
+        tr.addEventListener('click', function () {
+            prikazDetalja(artikli[i])
+        })
+
+
         let br = document.createElement("td")
         let naziv = document.createElement("td")
         let cena = document.createElement("td")
-        
+
         br.style.border = '1px solid black'
         naziv.style.border = '1px solid black'
         cena.style.border = '1px solid black'
@@ -35,11 +40,19 @@ function kreirajRed() {
         tabela.appendChild(tr)
     }
 }
+function prikazDetalja(artikal) {
+    document.querySelector("#detaljNaziv").textContent = "Naziv: " + artikal.naziv
+    document.querySelector("#detaljCena").textContent = "Cena: " + artikal.cena + "$"
+    document.querySelector("#detaljOpis").textContent = "Opis: " + artikal.opis
+}
+
+
 
 function inicijalizujArtikle() {
     artikli = [
-        new Artikal('Monitor', 165, 'Velicina: 24/"'),
-        new Artikal('Mis', 20, 'Bežični, optički')
+        new Artikal('Monitor', 165, '23.8" IPS ekran sa širokim uglovima gledanja (178°)'),
+        new Artikal('Mis', 20, 'Bežični, optički'),
+        new Artikal("TV",650, "Rezolucija: 4K Ultra HD 3840 x 2160p")
     ]
     kreirajRed()
 }
